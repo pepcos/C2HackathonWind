05fb29cfa6619172dccd65f9f4149fc2e78a3c42
Carlos Delgado-Torres <b381958@l40049.atos.local>
2022-06-29 17:35:30 +0200

    .

--- wind2power.py
+++ wind2power.py
@@ -12,7 +12,7 @@ def wind2power(wind, type):
         xp = [3,3.5,4,4.5,5,5.5,6,6.5,7,7.5,8,8.5,9,9.5,10,10.5,11,11.5,21.5,22]
         fp = [0.023,0.050,0.083,0.123,0.174,0.236,0.310,0.396,0.498,0.612,0.738,0.870,0.994,1.094,1.163,1.197,1.213,1.217,1.217,0.000]
     else:
-        raise ValueError('type must be high-wind or low-wind')
+        raise ValueError('type must be "high-wind" or "low-wind"')
     
     power = np.interp(x = wind, xp = xp, fp = fp, left = 0, right = 0)
     
